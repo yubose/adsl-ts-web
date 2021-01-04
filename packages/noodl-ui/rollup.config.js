35ca752d116f4005d0ccf097f6c23a34aff56c9b
pfftdammitchris <pfftdammitchris@gmail.com>
2021-01-03 22:39:00 -0800

    edited configs

--- packages/noodl-ui/rollup.config.js
+++ packages/noodl-ui/rollup.config.js
@@ -30,7 +30,6 @@ const config = {
     resolve({
       extensions,
       customResolveOptions: {
-        paths: ['node_modules'],
         moduleDirectory: ['node_modules'],
       },
     }),
@@ -45,7 +44,6 @@ const config = {
       include: ['src/**/*'],
       exclude: ['node_modules'],
       extensions,
-      sourceMaps: true,
     }),
     // Env var set by root lerna repo
     // ...(process.env.NODE_ENV !== 'development' ? [terser()] : []),
