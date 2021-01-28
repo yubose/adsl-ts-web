014ea6ca71119a092af78172d2f9054e2014e75e
Christopher Tran <pfftdammitchris@gmail.com>
2021-01-28 09:11:50 -0800

    updated sdk

---
+++ src/__tests__/testing.test.ts
@@ -0,0 +1,9 @@
+import NOODL from '@aitmed/cadl'
+import sinon from 'sinon'
+import { EventEmitter } from 'events'
+
+describe(`hello`, () => {
+  it(``, async () => {
+    console.info(NOODL)
+  })
+})
