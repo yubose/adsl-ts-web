47a0aafece09aee3be1fe150e1ec60dd1f9793c6
Christopher Tran <pfftdammitchris@gmail.com>
2021-05-14 09:41:41 -0700

    G

--- scripts/buildOrStart.js
+++ scripts/buildOrStart.js
@@ -1,5 +1,6 @@
-const { spawn, exec, execFile } = require('child_process')
-const chalk = require('chalk')
+const u = require('@jsmanifest/utils')
+const { spawn } = require('child_process')
+const color = require('./colors')
 
 /**
  *
@@ -8,34 +9,55 @@ const chalk = require('chalk')
 async function buildOrStart(props) {
   console.log('props', props)
 
+  const { config, input = [] } = props
+  const { deploy } = props.flags
+
+  let command = input[0] // 'start', 'build', etc
+  let lib = '' // Will be found using libInput
+  let libInput = input[1] // Regex / alias (ex: 'nui' will be computed to 'noodl-ui')
+  let isBuilding = command === 'build'
+  let isStart = command === 'start'
+  let isDeploying = deploy === true
+
   try {
-    let tag = start ? 'start' : build ? 'build' : test ? 'test' : 'test:file'
     let cmd = ``
     let cmdArgs = []
-    let lib = ``
 
-    for (const pair of libReg) {
-      const obj = pair.value
-      const regexStr = obj.get('regex')
-      const regex = new RegExp(regexStr, 'i')
-      if (regex.test(args[tag])) lib = pair.key.value
+    const aliases = config.op?.alias
+
+    if (libInput === 'app') {
+      // Web app
+      cmd += `npm`
+      cmdArgs.push('run')
+      if (isBuilding) {
+        cmdArgs.push(isDeploying ? 'build:deploy:test' : 'build:test')
+      } else if (isStarting) {
+        cmdArgs.push(`lib:start`)
+      }
+    } else {
+      // Local pkg
+      for (const [pkgName, { regex: regexStr }] of u.entries(aliases)) {
+        if (new RegExp(regexStr, 'i').test(libInput)) {
+          lib = pkgName
+          break
+        }
+      }
+
+      if (!lib) {
+        throw new Error(
+          `Required lib name for ${color.magenta(command)} script`,
+        )
+      }
+
+      cmd += `lerna`
+      cmdArgs.push(
+        'exec',
+        '--scope',
+        lib,
+        `\"npm run ${command}${args._.join(' ')}\"`,
+      )
     }
 
-    if (!lib && testfile) {
-      // lib = testfile.join(' ')
-    }
-
-    if (!lib) {
-      throw new Error(`Required lib name for ${chalk.magenta(tag)} script`)
-    }
-
-    cmd += `lerna`
-    cmdArgs.push(
-      'exec',
-      '--scope',
-      lib,
-      `\"npm run ${tag}${args._.join(' ')}\"`,
-    )
     spawn(cmd, cmdArgs, { stdio: 'inherit', shell: true })
   } catch (error) {
     throw new Error(error.message)
