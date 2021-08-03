1172617ccff2eeabad6529442c9b8eb95b1072e0
christopher@aitmed.com <pfftdammitchris@gmail.com>
2021-08-03 15:33:03 -0700

    fixed identify toast func problem

--- packages/noodl-ui/src/utils/getActionObjectErrors.ts
+++ packages/noodl-ui/src/utils/getActionObjectErrors.ts
@@ -8,7 +8,7 @@ function getActionObjectErrors(obj: NUIActionObjectInput | undefined) {
     //
   } else if (Identify.goto(obj)) {
     //
-  } else if (Identify.toast(obj)) {
+  } else if (Identify.toast?.(obj)) {
     //
   } else if (Identify.action.any(obj)) {
     if (obj.actionType === 'anonymous') {
