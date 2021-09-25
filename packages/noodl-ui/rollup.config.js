d328895a7cbf6b5eb6e39edac8c5997bd6b90d8e
Christopher Tran <pfftdammitchris@gmail.com>
2021-09-24 20:54:57 -0700

    G

--- packages/noodl-ui/rollup.config.js
+++ packages/noodl-ui/rollup.config.js
@@ -1,14 +1,10 @@
 import { DEFAULT_EXTENSIONS } from '@babel/core'
-import nodePolyfills from 'rollup-plugin-node-polyfills'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+// import nodePolyfills from 'rollup-plugin-node-polyfills'
 import filesize from 'rollup-plugin-filesize'
 import external from 'rollup-plugin-peer-deps-external'
 import progress from 'rollup-plugin-progress'
-// import babel from '@rollup/plugin-babel'
 import esbuild from 'rollup-plugin-esbuild'
-// import typescript from 'rollup-plugin-typescript2'
-// import { terser } from 'rollup-plugin-terser'
 
 const extensions = [...DEFAULT_EXTENSIONS, '.ts']
 const _DEV_ = process.env.NODE_ENV === 'development'
@@ -40,48 +36,23 @@ const configs = [
       },
     ],
     plugins: [
-      nodePolyfills(),
+      // nodePolyfills(),
       external(),
-      commonjs(),
       filesize(),
       progress(),
       nodeResolve({
-        browser: true,
+        // browser: true,
         extensions,
         moduleDirectories: ['node_modules'],
-        preferBuiltins: false,
+        // preferBuiltins: false,
       }),
-      // typescript({
-      //   rollupCommonJSResolveHack: true,
-      //   check: false,
-      //   abortOnError: false,
-      //   clean: true,
-      // }),
-      // babel({
-      //   babelHelpers: 'runtime',
-      //   include: ['src/**/*'],
-      //   exclude: ['node_modules/**/*'],
-      //   extensions: ['.js'],
-      // }),
-      // !_DEV_
-      //   ? terser({
-      //       compress: {
-      //         drop_console: false,
-      //         drop_debugger: false,
-      //       },
-      //       keep_fnames: true,
-      //       format: {
-      //         source_map: { includeSources: true },
-      //       },
-      //     })
-      //   : undefined,
       esbuild({
         include: /\.[t]s?$/,
         exclude: /node_modules/,
-        // minify: process.env.NODE_ENV === 'production',
-        minify: true,
+        minify: !_DEV_,
         target: 'es2018',
         sourceMap: true,
+        experimentalBundling: true,
       }),
     ],
   },
