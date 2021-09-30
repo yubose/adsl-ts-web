f2cd22a481a50898c672ee9358c58a457ffd68a8
christopher@aitmed.com <pfftdammitchris@gmail.com>
2021-09-30 10:17:36 -0700

    init sync script

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,24 +1,18 @@
 const execa = require('execa')
 const u = require('@jsmanifest/utils')
-const yaml = require('yaml')
 const fs = require('fs-extra')
 const path = require('path')
-const chalk = require('chalk')
 const cpy = require('cpy')
 
-const aqua = (s) => chalk.keyword('aquamarine')(s)
-const coolGold = (s) => chalk.keyword('navajowhite')(s)
-const tag = (s) => `[${u.cyan(s)}]`
 const log = console.log
-
 const normalizePath = (s = '') => s.replace(/\\/g, '/')
+const tag = (s) => `[${u.cyan(s)}]`
 
 /**
  *
  * @param { import('./op') } props
  */
 async function sync() {
-  const toDir = path.join(process.cwd(), './generated')
   const shellArgs = { shell: true, stdio: 'inherit' }
   const command = async (cmd = '') => execa(cmd, shellArgs)
 
@@ -48,16 +42,6 @@ async function sync() {
           path.resolve(process.cwd(), targetDir).replace(/\\/g, '/'),
         )
         targetAssetsDir = path.join(targetDir, 'assets')
-
-        // console.log({
-        //   repo,
-        //   baseDir,
-        //   appFiles,
-        //   configFrom,
-        //   targetDir,
-        //   configNames,
-        //   gitFolder,
-        // })
 
         if (fs.existsSync(baseDir)) {
           await command(`cd ${baseDir}`)
@@ -101,6 +85,17 @@ async function sync() {
 
   await syncRepos([
     {
+      url: 'http://gitlab.aitmed.com/production/superAdmin.git',
+      from: {
+        baseDir: '../superadmin',
+        appFiles: 'superadmin',
+        configFrom: 'config/sadmind.yml',
+      },
+      to: {
+        targetDir: 'generated/sadmind',
+      },
+    },
+    {
       url: 'http://gitlab.aitmed.com/production/admin.git',
       from: {
         baseDir: '../admind2',
@@ -115,193 +110,52 @@ async function sync() {
         targetDir: 'generated/admind2',
       },
     },
-    // {
-    //   from: {
-    //     url: 'http://gitlab.aitmed.com/production/search.git',
-    //     baseDir: '../../searchd2',
-    //     appFiles: 'search',
-    //     configFrom: [
-    //       'config/search.yml',
-    //       'config/searchd.yml',
-    //       'config/searchd2.yml',
-    //     ],
-    //   },
-    //   to: {
-    //     targetDir: 'generated/searchd2',
-    //   },
-    // },
-  ])
-
-  return
-
-  async function syncApp({ name, from, to, configPath }) {
-    const configFilePathFrom = path.resolve(path.join(from, configPath))
-    const configFilePathTo = path.resolve(path.join(to, configPath))
-
-    console.log(
-      `Syncing ${chalk.cyan(name)} from ${chalk.magenta(
-        from,
-      )} to ${chalk.magenta(to)}`,
-    )
-
-    try {
-      await fs.copy(from, to, {
-        errorOnExist: false,
-        preserveTimestamps: false,
-        overwrite: true,
-        recursive: true,
-        dereference: true,
-      })
-
-      console.log(
-        `${chalk.cyan(`Using`)} config from ${chalk.magenta(configPath)}`,
-      )
-
-      await fs.copy(configPath, configFilePathTo, { overwrite: true })
-
-      if (fs.existsSync(configPath)) {
-        const yml = await fs.readFile(configPath, 'utf8')
-        if (yml && u.isStr(yml)) {
-          const doc = yaml.parseDocument(yml)
-          if (doc) {
-            if (doc.has('cadlBaseUrl')) {
-              doc.set('cadlBaseUrl', `http://127:0.0.1:3001/`)
-              if (doc.has('myBaseUrl')) {
-                doc.set('myBaseUrl', `http://127:0.0.1:3001/`)
-              }
-              await fs.writeFile(
-                configFilePathTo,
-                yaml.stringify(doc, { indent: 2 }),
-                'utf8',
-              )
-            }
-          }
-        }
-      }
-    } catch (err) {
-      console.log('')
-      console.log(`[${chalk.yellow(err.name)}]: ${chalk.red(err.message)}`)
-      // throw new Error(err.message)
-    }
-  }
-
-  await syncApp({
-    name: 'search',
-    configPath: 'config/searchd2.yml',
-    from: '../aitmed-search/search',
-    to: './generated/searchd2',
-  })
-
-  await syncApp({
-    name: 'admin',
-    configPath: '../aitmed-admin/config/admind2.yml',
-    from: '../aitmed-admin/admin',
-    to: './generated/admind2',
-  })
-
-  await syncApp({
-    name: 'admin',
-    configPath: '../aitmed-admin/config/admind2.yml',
-    from: '../aitmed-sadmin/superadmin',
-    to: './generated/superadmin',
-    repo: 'http://gitlab.aitmed.com/production/superadmin.git',
-  })
-
-  await syncApp({
-    name: 'testpage',
-    configPath: '../cadl/config/testpage.yml',
-    from: '../cadl/testpage',
-    to: './generated/testpage',
-  })
-
-  const dirsToSync = [
-    '../cadl/meet2',
-    '../cadl/meet3',
-    '../cadl/www',
-    '../cadl2/patient',
-    '../cadl2/provider',
-  ].reduce((acc, dir) => {
-    const folder = dir.substring(dir.lastIndexOf('/') + 1)
-    let to = path.join(toDir, folder)
-    let toFolder = ''
-    if (folder === 'meet2') to = to.replace('meet2', 'meetd2')
-    if (folder === 'patient') to = to.replace('patient', 'patd2')
-    if (folder === 'provider') to = to.replace('provider', 'prod2')
-    toFolder = path.basename(to)
-    acc.push({
-      folder,
-      pathname: dir,
-      toFolder,
-      to,
-      configPath: path.join(dir, `../config/${folder}.yml`),
-    })
-    return acc
-  }, [])
-
-  if (!fs.existsSync(toDir)) {
-    fs.ensureDirSync(toDir)
-    console.log(
-      `${chalk.cyan(
-        `New directory`,
-      )} Created a new directory at ${chalk.magenta(toDir)}`,
-    )
-  }
-
-  await execa.command(
-    `cd ../cadl && git reset --hard origin/master && git pull -f && cd ../cadl2 && git reset --hard origin/master && git pull -f && cd ../aitmed-search && git reset --hard origin/master && git pull -f`,
     {
-      shell: true,
-      stdio: 'inherit',
+      url: 'http://gitlab.aitmed.com/production/search.git',
+      from: {
+        baseDir: '../searchd2',
+        appFiles: 'search',
+        configFrom: [
+          'config/search.yml',
+          'config/searchd.yml',
+          'config/searchd2.yml',
+        ],
+      },
+      to: {
+        targetDir: 'generated/searchd2',
+      },
     },
-  )
-
-  for (const obj of dirsToSync) {
-    let { configPath, folder, pathname, to, toFolder } = obj
-    console.log(`${chalk.cyan('Syncing')} from ${chalk.magenta(pathname)}`)
-
-    try {
-      await fs.copy(pathname, to, {
-        errorOnExist: false,
-        preserveTimestamps: false,
-        overwrite: true,
-        recursive: true,
-        dereference: true,
-      })
-
-      console.log(
-        `${chalk.cyan(`Syncing`)} config from ${chalk.magenta(configPath)}`,
-      )
-
-      const targetConfigPath =
-        path.join(toDir, toFolder, path.basename(to)) + '.yml'
-
-      await fs.copy(configPath, targetConfigPath, { overwrite: true })
-
-      if (fs.existsSync(targetConfigPath)) {
-        const yml = await fs.readFile(targetConfigPath, 'utf8')
-        if (yml && u.isStr(yml)) {
-          const doc = yaml.parseDocument(yml)
-          if (doc) {
-            if (doc.has('cadlBaseUrl')) {
-              doc.set('cadlBaseUrl', `http://127:0.0.1:3001/`)
-              if (doc.has('myBaseUrl')) {
-                doc.set('myBaseUrl', `http://127:0.0.1:3001/`)
-              }
-              await fs.writeFile(
-                targetConfigPath,
-                yaml.stringify(doc, { indent: 2 }),
-                'utf8',
-              )
-            }
-          }
-        }
-      }
-    } catch (err) {
-      console.log('')
-      console.log(`[${chalk.yellow(err.name)}]: ${chalk.red(err.message)}`)
-      // throw new Error(err.message)
-    }
-  }
+    {
+      url: 'http://gitlab.aitmed.com/production/aitmed.git',
+      from: {
+        baseDir: '../patd2',
+        appFiles: 'patient',
+        configFrom: [
+          'config/patient.yml',
+          'config/patd.yml',
+          'config/patd2.yml',
+        ],
+      },
+      to: {
+        targetDir: 'generated/patd2',
+      },
+    },
+    {
+      url: 'http://gitlab.aitmed.com/production/aitmed.git',
+      from: {
+        baseDir: '../prod2',
+        appFiles: 'provider',
+        configFrom: [
+          'config/provider.yml',
+          'config/prod.yml',
+          'config/prod2.yml',
+        ],
+      },
+      to: {
+        targetDir: 'generated/prod2',
+      },
+    },
+  ])
 }
 
 module.exports = sync
