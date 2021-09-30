46836fb251f4ac04e83f149121ff123d1b968b7c
YongjianYu <yongjian.yu@aitmed.com>
2021-09-30 09:34:24 +0800

    back

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -66,7 +66,7 @@ export function getVcodeElem(dataKey = 'formData.code') {
 }
 
 export const hide = makeElemFn(
-  (node) => node?.style && (node.style.display = 'none'),
+  (node) => node?.style && (node.style.visibility = 'hidden'),
 )
 export const show = makeElemFn((node) => {
   if (!node?.style) return
