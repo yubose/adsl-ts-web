640f32e5939a457baec5e090829c7697fa53ea6a
Christopher Tran <pfftdammitchris@gmail.com>
2021-12-15 19:08:34 -0800

    removed inactive dir

--- packages/nui/src/__tests__/test-utils.ts
+++
@@ -1,9 +0,0 @@
-import { actionFactory, componentFactory } from 'noodl-ui-test-utils'
-import Nui from '../nui'
-import NuiPage from '../Page'
-import Viewport from '../Viewport'
-
-export const assetsUrl = 'https://something.com/assets/'
-export const nui = Nui
-export const viewport = new Viewport()
-export const ui = { ...actionFactory, ...componentFactory }
