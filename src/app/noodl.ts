b9bc7e448c75eb84c4506fc2ac140e50bbb1f4a4
YongjianYu <yongjian.yu@aitmed.com>
2021-12-22 15:14:36 +0800

    update outside link

--- src/app/noodl.ts
+++ src/app/noodl.ts
@@ -28,7 +28,7 @@ function getConfigEndpoint(name: string) {
 //    ex ---> process.env.DEPLOYING ? SAFE_DEPLOY_URL : MEET2D
 //    ex ---> process.env.DEPLOYING ? SAFE_DEPLOY_URL : LOCAL_SERVER
 
-const CONFIG_URL = process.env.DEPLOYING ? SAFE_DEPLOY_URL : LOCAL_SERVER
+const CONFIG_URL = '../cadl/config/LocalDev.yml'
 // Local server
 // const CONFIG_URL = `../local.yml`
 let noodl: NOODL | undefined
