67ee2b9ff14d27bfaba278d78e46447066d42b90
dazhou.tong@aitmed.com <3286614219@qq.com>
2022-09-29 14:48:14 +0800

    update package

--- apps/web/src/app/noodl.ts
+++ apps/web/src/app/noodl.ts
@@ -25,10 +25,7 @@ export const lvl3Options = {
     // if (process.env.LOCAL_CONFIG_URL) return process.env.LOCAL_CONFIG_URL
     // This will be returned (normal use) if NOT using -- env APP=<config name>
     const port = 3000
-    return isDeploying
-    ? safeDeployUrl
-    : `http://127.0.0.1:${port}/${lvl3Options.app}.yml`
-
+    return safeDeployUrl
     // return `./analysis.yml`
   },
 }
