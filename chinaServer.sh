e03a17b7936cb30564a36b3bb0937d2480089a6b
xuchen-imac <chenxu2656@gmail.com>
2022-11-14 12:38:54 +0800

    update cn serve deploy

--- chinaServer.sh
+++ chinaServer.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-scp -r output/apps/web/* root@47.97.40.231:/static
+# https://help.aliyun.com/document_detail/50452.html?spm=5176.8466035.help.dexternal.7e001450AfhsTr
+~/ossutilmac64 rm oss://aitmed-webapp/ -r
+~/ossutilmac64 cp -r output/apps/web/ oss://aitmed-webapp/
