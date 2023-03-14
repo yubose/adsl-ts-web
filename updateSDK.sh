3bc23d8d4e137594f6099b46cedd9f64f9a2e866
xuchen-imac <chenxu2656@gmail.com>
2023-03-14 02:43:56 -0700

    update aitmed sdk, inlcude lvl2,lvl3,noodl-ui

--- updateSDK.sh
+++ updateSDK.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-npm install @aitmed/cadl@1.0.868-4
-npm install @aitmed/ecos-lvl2-sdk@1.4.226
-npm i noodl-ui@0.1.1114
+npm install @aitmed/cadl@latest
+npm install @aitmed/ecos-lvl2-sdk@latest
+npm i noodl-ui@latest
 npm i noodl-types@latest
 
 cd apps/web
-npm install @aitmed/cadl@1.0.868-4
-npm install @aitmed/ecos-lvl2-sdk@1.4.226
-npm i noodl-ui@0.1.1114
+npm install @aitmed/cadl@latest
+npm install @aitmed/ecos-lvl2-sdk@latest
+npm i noodl-ui@latest
 npm i noodl-types@latest
 
-# git add .
-# git commit -a -m "update aitmed sdk, inlcude lvl2,lvl3,noodl-ui"
-# git push
+git add .
+git commit -a -m "update aitmed sdk, inlcude lvl2,lvl3,noodl-ui"
+git push
 
 
 
