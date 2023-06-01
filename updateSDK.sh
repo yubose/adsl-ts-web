72d82db3be74046abf9fb38e5b63d95c5874a110
shitong <shitong@aitmed.com>
2023-06-01 13:58:55 +0800

    update aitmed sdk, inlcude lvl2,lvl3,noodl-ui

--- updateSDK.sh
+++ updateSDK.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-npm install @aitmed/cadl@1.4.14
-npm install @aitmed/ecos-lvl2-sdk@1.4.244
+npm install @aitmed/cadl@latest
+npm install @aitmed/ecos-lvl2-sdk@latest
 npm i noodl-ui@latest
 npm i noodl-types@latest
 
 cd apps/web
-npm install @aitmed/cadl@1.4.14
-npm install @aitmed/ecos-lvl2-sdk@1.4.244
+npm install @aitmed/cadl@latest
+npm install @aitmed/ecos-lvl2-sdk@latest
 npm i noodl-ui@latest
 npm i noodl-types@latest
 
