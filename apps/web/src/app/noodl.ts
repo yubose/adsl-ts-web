9ab104ee733ef87c69c5008f01fb999fe25d09eb
shitong <shitong@aitmed.com>
2023-06-15 11:29:27 +0800

    update aitmed sdk, inlcude lvl2,lvl3,noodl-ui

--- apps/web/src/app/noodl.ts
+++ apps/web/src/app/noodl.ts
@@ -26,7 +26,8 @@ export const lvl3Options = {
     // if (process.env.LOCAL_CONFIG_URL) return process.env.LOCAL_CONFIG_URL
     // This will be returned (normal use) if NOT using -- env APP=<config name>
     const port = 3000
-    return safeDeployUrl
+    // return safeDeployUrl
+    return '../local.yml'
     // return isDeploying
     //   ? safeDeployUrl
     //   : `http://127.0.0.1:${port}/${lvl3Options.app}.yml`
