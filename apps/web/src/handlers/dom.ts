79fd0d27b1873bdc12ccf5c3d18a731e9043c482
shitong <shitong@aitmed.com>
2023-07-10 17:56:14 +0800

    update

--- apps/web/src/handlers/dom.ts
+++ apps/web/src/handlers/dom.ts
@@ -4461,7 +4461,6 @@ const createExtendedDOMResolvers = function (app: App) {
             });
             (document.getElementById("preView") as HTMLDivElement).innerHTML = html
             oldSHA = newSHA
-            console.log(Object.keys(app.root.Global.formData.templateInfo.yaml.data).length)
           }
         }
         editorConfig.onChange = change
