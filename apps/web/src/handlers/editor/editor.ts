fbd6e96cdd8f113c4c6191291d5d4e3f348ae2ca
shitong <shitong@aitmed.com>
2023-07-21 17:12:52 +0800

    update template-paste-image

--- apps/web/src/handlers/editor/editor.ts
+++ apps/web/src/handlers/editor/editor.ts
@@ -1,4 +1,5 @@
 import { IEditorConfig, IDomEditor, SlateTransforms  } from "@wangeditor/editor"
+import { getUuid } from "./utils/utils"
 
 type InsertFnType = (url: string, alt: string, href: string) => void
 
@@ -42,7 +43,7 @@ const editorConfig: Partial<IEditorConfig> = {
     },
     customPaste: (editor: IDomEditor, event: ClipboardEvent): boolean => {
         let text = event.clipboardData?.getData("text/plain")
-        console.log(text)
+        let image = event.clipboardData?.items?.[0].getAsFile()
         if(text && text !== "") {
             try {
                 const nodes = JSON.parse(text)
@@ -57,9 +58,34 @@ const editorConfig: Partial<IEditorConfig> = {
                 })
             }
             return false
-        } else {
-            return true
         }
+        if(!!image) {
+            try {
+                const reader = new FileReader()
+                reader.onload = e => {
+                    console.log(e.target?.result)
+                    const node = {
+                        type: "image",
+                        alt: getUuid(),
+                        src: e.target?.result,
+                        href: '',
+                        children: [
+                            {text: ''}
+                        ]
+                    }
+                    console.log("START")
+                    editor.focus()
+                    editor.insertNode(node)
+                    console.log("END")
+                }
+                reader.readAsDataURL(image)
+                console.log(image)
+            } catch (error) {
+                
+            }
+            return false
+        }
+        return true
     }
 }
 
