23d7154f59be2f981a0729ac2022e93343c69d35
xuchen-imac <chenxu2656@gmail.com>
2023-08-29 18:47:08 +0800

    update aitmed sdk, inlcude lvl2,lvl3,noodl-ui

--- chinaServer.sh
+++ chinaServer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # https://help.aliyun.com/document_detail/50452.html?spm=5176.8466035.help.dexternal.7e001450AfhsTr
-ossutil rm oss://aitmed-webapp/ -r
-ossutil cp -r output/apps/web/ oss://aitmed-webapp/
-# ossutil64 rm oss://aitmed-webapp/ -r
-# ossutil64 cp -r output/apps/web/ oss://aitmed-webapp/
+# ossutil rm oss://aitmed-webapp/ -r
+# ossutil cp -r output/apps/web/ oss://aitmed-webapp/
+ossutil64 rm oss://aitmed-webapp/ -r
+ossutil64 cp -r output/apps/web/ oss://aitmed-webapp/
