2462d48387b492ae4a2aa5cc1b781ab3330e8778
dazhou.tong <dazhou.tong@aitmed.com>
2023-10-11 10:06:42 +0800

    update shearState

--- apps/web/src/handlers/actions.ts
+++ apps/web/src/handlers/actions.ts
@@ -723,9 +723,9 @@ const createActions = function createActions(app: App) {
       rootDom.appendChild(divRootDom)
       img.src = URL.createObjectURL(file as Blob) as string
       cropper = new Cropper(img, {
-        viewMode: 2, // 只能在裁剪的图片范围内移动
+        viewMode: 1, // 只能在裁剪的图片范围内移动
         dragMode: 'move', // 画布和图片都可以移动
-        aspectRatio: 1, // 裁剪区默认正方形
+        // aspectRatio: 1, // 裁剪区默认正方形
         autoCropArea: 1, // 自动调整裁剪图片
         zoomOnWheel: true,
         scalable: true,
