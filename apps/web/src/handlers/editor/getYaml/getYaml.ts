aec03b4c644a70208f57cdefdf29c78e0e6579ee
shitong <shitong@aitmed.com>
2023-11-01 10:48:21 +0800

    update provider signature

--- apps/web/src/handlers/editor/getYaml/getYaml.ts
+++ apps/web/src/handlers/editor/getYaml/getYaml.ts
@@ -517,6 +517,17 @@ const populateBlock = ({
                     } else if(obj.value === "#Provider Signature") {
                         const text = obj.value.replace(/#/, '')
                         BaseJsonCopy.formData["signatureId"] = ''
+                        BaseJsonCopy.formData["providerSignature"] = {
+                            SignersName: "",
+                            SignersSpecialty: "",
+                            SignersLICNumber: "",
+                            SignersNPINumber: "",
+                            SignersDEANumber: "",
+                            SignersEmail: "",
+                            FacilityFaxNumber: "",
+                            MainNumber: "",
+                            FacilityAddress: ""
+                        }
                         target = sharpYaml({
                             type: text,
                             config: {}
