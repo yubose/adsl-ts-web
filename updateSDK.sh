23d8b1b03bcf26610f58af85fe0979f4979f4030
xuchen-imac <chenxu2656@gmail.com>
2023-12-13 17:02:28 +0800

    release new production version

--- updateSDK.sh
+++ updateSDK.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-npm install @aitmed/cadl@1.7.19-7
+npm install @aitmed/cadl@1.7.19-8
 npm install @aitmed/ecos-lvl2-sdk@1.4.256
 npm i noodl-ui@0.1.1176
 npm i noodl-types@1.0.273
 
 cd apps/web
-npm install @aitmed/cadl@1.7.19-7
+npm install @aitmed/cadl@1.7.19-8
 npm install @aitmed/ecos-lvl2-sdk@1.4.256
 npm i noodl-ui@0.1.1176
 npm i noodl-types@1.0.273
