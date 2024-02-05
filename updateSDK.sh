290379a02a0b0c0961e1d44f42d63748b060a585
xuchen-imac <chenxu2656@gmail.com>
2024-02-05 16:22:53 +0800

    release a new production version

--- updateSDK.sh
+++ updateSDK.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 npm install @aitmed/cadl@latest -f
-npm install @aitmed/ecos-lvl2-sdk@latest -f
+npm install @aitmed/ecos-lvl2-sdk@1.4.260 -f
 npm i noodl-ui@0.1.1190 -f
-npm i noodl-types@latest -f
+npm i noodl-types@1.0.277 -f
 
 cd apps/web
 npm install @aitmed/cadl@latest -f
-npm install @aitmed/ecos-lvl2-sdk@latest -f
+npm install @aitmed/ecos-lvl2-sdk@1.4.260 -f
 npm i noodl-ui@0.1.1190 -f
-npm i noodl-types@latest -f
+npm i noodl-types@1.0.277 -f
 
 # git add .
 # git commit -a -m "update aitmed sdk, inlcude lvl2,lvl3,noodl-ui"
